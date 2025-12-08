ad7b1ad997a5fcdcf27b3f303a16f3671937c079
Nhat <trinhthanhnhat123@gmail.com>
2025-12-09 03:09:19 +0700

    cors

--- src/main/java/com/web/web/Config/CorsConfig.java
+++ src/main/java/com/web/web/Config/CorsConfig.java
@@ -15,8 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // Áp dụng cho tất cả API dưới /api
                         .allowedOrigins(
-                                "https://nhat.cloud",
-                                "https://www.nhat.cloud") // Thêm origin của Front-end
+                                "https://nhat.cloud") // Thêm origin của Front-end
                         .allowedMethods("*")// Thêm origin của Front-end
                         .allowedHeaders("*") // Các phương thức được phép "GET", "POST", "PUT", "DELETE", "OPTIONS"
                         .allowCredentials(true);// Cho phép cookie, authorization headers, v.v.
