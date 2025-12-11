b1ca47cb7594e6d6d5c6afb589bf55ecb8c19939
Nhat <trinhthanhnhat123@gmail.com>
2025-12-11 23:49:18 +0700

    fix(api): err upload img

--- src/main/java/com/web/web/Config/CorsConfig.java
+++ src/main/java/com/web/web/Config/CorsConfig.java
@@ -13,11 +13,14 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Áp dụng cho tất cả API dưới /api
+                registry.addMapping("/**") // Áp dụng cho tất cả API
                         .allowedOrigins(
-                                "https://nhat.cloud") // Thêm origin của Front-end
-                        .allowedMethods("*")// Thêm origin của Front-end
-                        .allowedHeaders("*") // Các phương thức được phép "GET", "POST", "PUT", "DELETE", "OPTIONS"
+                                "https://nhat.cloud",
+                                "https://api.nhat.cloud",
+                                "http://localhost:5173",
+                                "http://localhost:3000") // Thêm các origin của Front-end
+                        .allowedMethods("*")// Các phương thức được phép "GET", "POST", "PUT", "DELETE", "OPTIONS"
+                        .allowedHeaders("*") // Cho phép tất cả headers
                         .allowCredentials(true);// Cho phép cookie, authorization headers, v.v.
             }
         };
